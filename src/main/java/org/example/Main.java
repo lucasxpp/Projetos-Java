82186e3782b48a106f1558ad7120d07333cc9fe5
Lucas Lima <lucaslopessxx@gmail.com>
2026-04-19 18:23:59 -0300

    novo commit agora

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,51 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+public class Main {
+
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5432/projectJ";
+        String user = "postgres";
+        String password = "33633599";
+
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Conectado com sucesso!");
+            Statement stm = conn.createStatement();
+            consultaDados(stm);
+
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("Erro na conexão:");
+            e.printStackTrace();
+
         }
     }
+
+static void consultaDados(Statement stm){
+        String sql = "select id, nome, email from usuario";
+        try {
+            ResultSet result = stm.executeQuery(sql);
+            while(result.next()){
+                System.out.printf("id: %s \n nome : %s \n email: %s \n", result.getInt("id"), result.getString("nome"), result.getString("email"));
+            }
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+
+
+}
+
+
+
+
+
+
 }
